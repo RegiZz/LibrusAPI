43dd1afe9609ae11a1ebc0850c8c7f4abe071c17
RegiZz <bartek.mendrzycki@o2.pl>
2024-10-17 17:08:55 +0200

    Dostosowanie do serwera czesc dalsza

--- src/main/java/api/librus/librusapi/LibrusLoginService.java
+++ src/main/java/api/librus/librusapi/LibrusLoginService.java
@@ -1,5 +1,6 @@
 package api.librus.librusapi;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,37 +14,39 @@ public class LibrusLoginService {
     // Metoda logująca użytkownika do systemu Librus
     public WebDriver login(String username, String password) throws InterruptedException {
 
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
+
         try {
-            // Ustawienia dla Selenium WebDriver
-            System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless");
-            options.addArguments("--no-sandbox"); // Bypass OS security model
-            options.addArguments("--disable-dev-shm-usage"); // Overcome limited resource problems
+            if(driver == null){
+                throw new Exception("Nie udało się stworzyć WebDrivera Chrome");
+            }else{
+                driver.get("https://portal.librus.pl/rodzina/login");
 
-            driver = new ChromeDriver(options);
-            System.out.println("WebDriver został zainicjalizowany");
+                // Logowanie
+                WebElement loginField = driver.findElement(By.name("login"));
+                WebElement passwordField = driver.findElement(By.name("password"));
+                loginField.sendKeys(username);
+                passwordField.sendKeys(password);
 
-            driver.get("https://portal.librus.pl/rodzina/login");
-            System.out.println("Zaladowano strone logowania");
+                WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
+                loginButton.click();
 
-            // Logowanie
-            WebElement loginField = driver.findElement(By.name("login"));
-            WebElement passwordField = driver.findElement(By.name("password"));
-            loginField.sendKeys(username);
-            passwordField.sendKeys(password);
+                // Czekaj na zalogowanie
+                Thread.sleep(2000);
+                return driver; // Zwracamy WebDriver, który będzie używany w dalszej części aplikacji
 
-            WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
-            loginButton.click();
-
-            // Czekaj na zalogowanie
-            Thread.sleep(2000);
-
-            return driver; // Zwracamy WebDriver, który będzie używany w dalszej części aplikacji
+            }
         }catch (Exception e) {
             e.printStackTrace();
+            driver.quit();
+            return null;
         }
-        return null;
+
     }
 
     public void quit() {
