32dbb36cfcd571947f8677203c4e6e76ca1125fc
RegiZz <bartek.mendrzycki@o2.pl>
2024-10-21 20:38:54 +0200

    Totalna zmiana kodu

--- src/main/java/api/librus/librusapi/LibrusController.java
+++ src/main/java/api/librus/librusapi/LibrusController.java
@@ -1,47 +1,41 @@
 package api.librus.librusapi;
 
-import org.openqa.selenium.WebDriver;
+import api.librus.librusapi.LibrusTimetableService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Map;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/librus")
 public class LibrusController {
 
-    @Autowired
-    private LibrusLoginService librusLoginService;
+    private final LibrusLoginService librusLoginService;
+    private final LibrusTimetableService librusTimetableService;
 
     @Autowired
-    private PlanLekcjiService planLekcjiService;
-
-    static class LoginRequest {
-        public String username;
-        public String password;
+    public LibrusController(LibrusLoginService librusLoginService, LibrusTimetableService librusTimetableService) {
+        this.librusLoginService = librusLoginService;
+        this.librusTimetableService = librusTimetableService;
     }
 
-    @PostMapping("/api/planlekcji")
-    public List<Map<String, String>> getPlanLekcji(@RequestBody LoginRequest loginRequest) throws InterruptedException {
-        WebDriver driver = null;
-        List<Map<String, String>> planLekcji = null;
-
-        try {
-            // Logowanie do Librusa
-            driver = librusLoginService.login(loginRequest.username, loginRequest.password);
-
-            // Pobieranie planu lekcji
-            planLekcji = planLekcjiService.getPlanLekcji(driver);
-
-        } finally {
-            // Zamykanie WebDrivera
-            if (driver != null) {
-                librusLoginService.quit();
-            }
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestParam String login, @RequestParam String password) {
+        String token = librusLoginService.login(login, password);
+        if (token != null) {
+            return ResponseEntity.ok(token);
+        } else {
+            return ResponseEntity.status(401).body("Nie udało się zalogować");
         }
+    }
 
-        return planLekcji;
+    @GetMapping("/timetables")
+    public ResponseEntity<Object> getTimetables(@RequestHeader("Authorization") String token) {
+        try {
+            LibrusTimetableService timetableService = new LibrusTimetableService(token);
+            Object timetables = timetableService.getTimetables();
+            return ResponseEntity.ok(timetables);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Wystąpił błąd podczas pobierania planu lekcji");
+        }
     }
 }
---
+++ src/main/java/api/librus/librusapi/LibrusTimetableService.java
@@ -0,0 +1,37 @@
+package api.librus.librusapi;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.ResponseEntity;
+
+@Service
+public class LibrusTimetableService {
+    private final String apiUrl = "https://api.librus.pl"; // Zaktualizuj URL, jeśli to konieczne
+    private final String token;
+
+    public LibrusTimetableService(String token) {
+        this.token = token;
+    }
+
+    public Object getTimetables() {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            String url = apiUrl + "/Timetables";
+
+            // Dodaj token do nagłówków
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "Bearer " + token);
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+
+            ResponseEntity<Object> response = restTemplate.exchange(url, HttpMethod.GET, entity, Object.class);
+
+            return response.getBody();
+        } catch (Exception e) {
+            System.out.println("[LibrusAPI: Timetables] Szkoła nie obsługuje tej funkcji");
+            throw e;
+        }
+    }
+}
